74d1198bb16e5c1fa74167022fc8c49ae9d5595a
Pr0v0k4tor <provokatoror@gmail.com>
2026-03-17 15:50:03 +0900

    Delete Проверочная работа 1.asm

--- Проверочная работа 1.asm
+++
@@ -1,9 +0,0 @@
-0x7fff069910d0                  cc                       int3
-0x7fff069910d1  <+    1>        c3                       ret
-0x7fff069910d2  <+    2>        cc                       int3
-0x7fff069910d3  <+    3>        cc                       int3
-0x7fff069910d4  <+    4>        cc                       int3
-0x7fff069910d5  <+    5>        cc                       int3
-0x7fff069910d6  <+    6>        cc                       int3
-0x7fff069910d7  <+    7>        cc                       int3
-0x7fff069910d8  <+    8>        0f 1f 84 00 00 00 00 00  nopl   0x0(%rax,%rax,1)
